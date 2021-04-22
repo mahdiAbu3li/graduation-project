1363739d3f7ec2dbd41b97566b6d6e8049ca37ee
mahdi abdulkareem <mahdiimad64@gmail.com>
2021-04-22 20:34:56 +0300

    finish upload images

--- src/component/CreateNewModel/UploadImages/UploadImages.tsx
+++ src/component/CreateNewModel/UploadImages/UploadImages.tsx
@@ -1,67 +1,93 @@
-import { stringify } from "node:querystring";
 import React, { useState } from "react";
 import styles from "./UploadImagesStyles.module.css";
+import uploadGif from "../../../assets/images/upload.gif";
+import loadingGif from "../../../assets/images/loading.gif";
 function UploadImages() {
-  const v = window.localStorage.getItem("values");
-  const vj = v !== null ? JSON.parse(v) : [];
-  const [files, setFiles] = useState<unknown[]>(vj);
-  function readFileAsText(file: File) {
-    return new Promise(function (resolve, reject) {
-      let fr = new FileReader();
+  const [files, setFiles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [uploadComponent, setUploadComponent] = useState<JSX.Element>();
+  console.log(isLoading);
 
-      fr.onload = function () {
-        resolve(fr.result);
-      };
+  const loading = <img src={loadingGif} alt="" />;
+  const uploading = <img src={uploadGif} alt="ad" style={{ width: "70%" }} />;
+  const [x, setx] = useState(false);
+  React.useEffect(() => {
+    const url = "https://graduationprojectt.herokuapp.com/api/dataset/55";
+    setIsLoading(true);
+    setUploadComponent(loading);
+    fetch(url, {
+      method: "get",
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        if (data.resources.length > 0) {
+          console.log(data.resources.length, "gett");
+          setFiles(data.resources);
+        } else return false;
+      });
+  }, [x]);
 
-      fr.onerror = function () {
-        reject(fr);
-      };
-
-      fr.readAsDataURL(file);
+  const uploadfile = async (data: FormData) => {
+    const url = "https://graduationprojectt.herokuapp.com/api/dataset/55";
+    const response = await fetch(url, {
+      method: "post",
+      mode: "cors",
+      body: data,
     });
-  }
-
+    console.log(response);
+    setIsLoading(false);
+    setx(!x);
+  };
   const imageSelectedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsLoading(true);
+
+    setUploadComponent(uploading);
+    console.log("mahdi");
     if (e.target.files !== null) {
       let files = e.currentTarget.files;
-      let readers = [];
-
-      // Abort if there were no files selected
-      if (!files?.length) return;
-
-      // Store promises in array
-      for (let i = 0; i < files.length; i++) {
-        readers.push(readFileAsText(files[i]));
+      const data = new FormData();
+      if (files === null) return;
+      for (let i in e.target.files) {
+        data.append("images[]", e.target.files[i]);
       }
-
-      // Trigger Promises
-      Promise.all(readers).then((values) => {
-        // Values will be an array that contains an item
-        // with the text of every selected file
-        // ["File1 Content", "File2 Content" ... "FileN Content"]
-        // if (typeof values !== "object") return;
-        // console.log(typeof values, 111111111111);
-        window.localStorage.setItem("values", JSON.stringify(values));
-        console.log(values, typeof values);
-        setFiles(values);
-      });
+      uploadfile(data);
+      // setUploadComponent(uploading);
     }
   };
-  console.log(files, 123132131);
 
-  const images = files?.map((file) => (
-    <img src={typeof file === "string" ? file : ""} alt="preview" />
+  const images = files?.map((file: { url: string }) => (
+    <img src={file.url} alt="preview" />
   ));
+
   return (
     <div className={styles.uploadContainer}>
       <div className={styles.uploadContainer_header}>
         <h3>Upload Images</h3>
         <p>_ upload image , model will learn from</p>
       </div>
+
       <div></div>
+      {/* <DropZone /> */}
       <div className={styles.wrapInput}>
         <div className={styles.hiddenLayer}>
-          {files.length !== 0 ? (
+          {/* {files.length !== 0 ? (
+            <div className={styles.image_container}>{images}</div>
+          ) : isLoading === false ? (
+            <p>
+              Drag and Drop or <span> Click here </span> to upload images
+            </p>
+          ) : (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {uploadComponent}
+            </div>
+          )} */}
+          {isLoading ? (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              {uploadComponent}
+            </div>
+          ) : files.length !== 0 ? (
             <div className={styles.image_container}>{images}</div>
           ) : (
             <p>
